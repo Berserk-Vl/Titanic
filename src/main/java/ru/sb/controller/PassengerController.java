64d8afa0b5358efc5ba17e01b5d3740fc21840df
Berserk-Vl <berkut.is.back@gmail.com>
2024-07-26 01:23:07 +0300

    Added @CrossOrigin annotation for "/passengers" mapping.

--- src/main/java/ru/sb/controller/PassengerController.java
+++ src/main/java/ru/sb/controller/PassengerController.java
@@ -2,6 +2,7 @@ package ru.sb.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +21,7 @@ public class PassengerController {
         this.passengerService = passengerService;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/passengers")
     ResponseEntity<Map<String, Object>> getPassengers(@RequestParam Map<String, String> queryParameters) {
         Stream<Passenger> passengerStream = passengerService.findAll().stream();
