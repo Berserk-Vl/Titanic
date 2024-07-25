0981e9b14489b6705dc79b2fde5945b908a32efc
Berserk-Vl <berkut.is.back@gmail.com>
2024-07-26 01:31:49 +0300

    Added interface passenger.

---
+++ titanic-app/src/app/passenger.ts
@@ -0,0 +1,11 @@
+export interface Passenger {
+    id: number;
+    survived: boolean;
+    pclass: string;
+    name: string;
+    sex: string;
+    age: number;
+    siblingsSpouses: number;
+    parentsChildren: number;
+    fare: number;
+}
